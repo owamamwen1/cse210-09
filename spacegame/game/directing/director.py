7f6590c678ea7c68616e0cadf7a749640938cc08
André Matheus Pedrosa <andre.matheus.pedrosa@gmail.com>
2022-03-29 14:34:41 -0300

    Added collision detection

--- spacegame/game/directing/director.py
+++ spacegame/game/directing/director.py
@@ -105,6 +105,10 @@ class Director:
         enemies = cast.get_actors("enemies")
         for enemy in enemies:
             enemy.move_next(max_x, max_y)
+            for bullet in player_bullets:
+                if (self.check_collision(bullet, enemy)):
+                    cast.remove_actor("player_bullets", bullet)
+                    cast.remove_actor("enemies", enemy)
 
     # The game over
 
@@ -119,3 +123,51 @@ class Director:
         """
         actors = cast.get_all_actors()
         self._display_service.draw_actors(actors)
+
+    def check_collision(self, actor1, actor2):
+        """"""
+        # Top actor 1
+        top_1 = actor1.get_position().get_y()
+        # Bottom actor 1
+        bottom_1 = top_1 + actor1.get_image_height()
+        # Left actor 1
+        left_1 = actor1.get_position().get_x()
+        # Right actor 1
+        right_1 = left_1 + actor1.get_image_width()
+
+
+        # Top actor 2
+        top_2 = actor2.get_position().get_y()
+        # Bottom actor 2
+        bottom_2 = top_2 + actor2.get_image_height()
+        # Left actor 2
+        left_2 = actor2.get_position().get_x()
+        # Right actor 2
+        right_2 = left_2 + actor2.get_image_width()
+
+        #print("top_1 " + str(top_1) + "   bottom_1 " + str(bottom_1) + "   left_1 " + str(left_1) + "   right_1 " + str(right_1) + "   top_2 " + str(top_2) + "   bottom_2 " + str(bottom_2) + "   left_2 " + str(left_2) + "   right_2 " + str(right_2))
+
+        if (actor1.get_image_height() > actor2.get_image_height()):
+            temp = top_1
+            top_1 = top_2
+            top_2 = temp
+            temp = bottom_1
+            bottom_1 = bottom_2
+            bottom_2 = temp
+
+        if (actor1.get_image_width() > actor2.get_image_width()):
+            temp = left_1
+            left_1 = left_2
+            left_2 = temp
+            temp = right_1
+            right_1 = right_2
+            right_2 = temp
+
+        check1 = top_1 >= top_2 and top_1 <= bottom_2
+        check2 = right_1 <= right_2 and right_1 >= left_2
+        check3 = bottom_1 >= top_2 and bottom_1 <= bottom_2
+        check4 = left_1 <= right_2 and left_1 >= left_2
+
+        #print(str(check1) + " " + str(check2) + " " + str(check3) + " " + str(check4))
+
+        return ((check1 and check2) or (check2 and check3) or (check3 and check4) or (check4 and check1))
